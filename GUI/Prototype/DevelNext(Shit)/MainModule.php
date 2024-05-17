10eca6bd022208ff0e68561f3a7e03a30cc15726
Ametero(z) <101631790+Ameterius@users.noreply.github.com>
2024-05-17 21:41:51 +0700

    Фастовый Прототипный ГУИ на говнопхп (Devel Next)

---
+++ GUI/Prototype/DevelNext(Shit)/MainModule.php
@@ -0,0 +1,10 @@
+<?php
+namespace ifuwml\modules;
+
+use std, gui, framework, ifuwml;
+
+
+class MainModule extends AbstractModule
+{
+
+}
---
+++ GUI/Prototype/DevelNext(Shit)/src/ifuwml/modules/AppModule.php
@@ -0,0 +1,18 @@
+<?php
+namespace ifuwml\modules;
+
+use std, gui, framework, ifuwml;
+
+
+class AppModule extends AbstractModule
+{
+
+    /**
+     * @event hotkey.action 
+     */
+    function doHotkeyAction(ScriptEvent $e = null)
+    {    
+        
+    }
+
+}
